06e7cd816c12168a9bf9c7d8d8491775d11bcaa1
dhanarrizky <dhanarrizky23@gmail.com>
2024-07-24 21:11:07 +0700

    creating inpOut code

---
+++ InpOut/inpOut.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+        string inp;
+
+        getline(cin, inp);
+
+        return 0;
+}
---
+++ sumDigPow/main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout << "out" << endl;
+	return 0;
+}
